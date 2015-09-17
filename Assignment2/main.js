546fca6635fedfa932983200905375915e72e2a5
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-09-17 12:55:05 -0700

    Removed javascript from HTML file and populated main.js with the initializations.

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -1,1 +1,10 @@
-﻿
+﻿"use strict";
+
+//Initialize HTML fields and forms.
+document.getElementById("rooms").innerHTML = "Rooms:   " + getRooms();
+document.getElementById("guests").innerHTML = "Guests:  " + getGuests();
+document.getElementById("weapons").innerHTML = "Weapons: " + getWeapons();
+
+populateSelectBox("RoomSelect", getRooms());
+populateSelectBox("SuspectSelect", getGuests());
+populateSelectBox("WeaponSelect", getWeapons());

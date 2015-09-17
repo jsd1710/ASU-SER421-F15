de5d55fe65cd03a23cf85a7d7d4424ea0c76531e
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-09-17 12:52:18 -0700

    Implemented basic data display for Clue Game

---
+++ Assignment2/functions/document.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+function populateSelectBox(boxID, itemArray)
+{
+    let destSelBox = document.getElementById(boxID);
+    let fragment = document.createDocumentFragment();
+
+    itemArray.forEach(
+        function (option, index)
+        {
+            let opt = document.createElement("option");
+            opt.innerHTML = option;
+            opt.value = option;
+            fragment.appendChild(opt);
+        }
+    );
+    destSelBox.appendChild(fragment);
+}
+
+
+
+
+
---
+++ Assignment2/functions/gameData.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+var GAME_PIECES_FILES = "../data/gamePieces.json";
+var gameJSON;
+
+function getRooms()
+{
+    return gameJSON["Rooms"];
+}
+
+function getGuests()
+{
+    return gameJSON["Guests"];
+}
+
+function getWeapons() {
+    return gameJSON["Weapons"];
+}
+
+function getClueJSON()
+{
+    let request = new XMLHttpRequest();
+    request.open("GET", GAME_PIECES_FILES, false);
+    request.send(null);
+    gameJSON = JSON.parse(request.responseText);
+}
+
+//Initialization
+getClueJSON();

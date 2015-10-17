b956b5c63d01a83c05094a62407e762b09128aef
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-10-17 12:06:15 -0700

    Added basic table and prototype for City

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,1 +1,32 @@
-﻿
+﻿var cities = {};
+
+init();
+
+function init()
+{
+    var DOMcities = document.getElementsByClassName("City");
+    for (i = 0; i < DOMcities.length; i++)
+    {
+        
+        cities[i] = new City(DOMcities[i].children.name.innerHTML);
+        console.log(cities[i].getName());
+    }
+}
+
+function City(name)
+{
+    this.city = {};
+
+    this.city["name"] = name;
+
+    this.getName = function()
+    {
+        return this.city["name"];
+    }
+
+    this.getTemp = function()
+    {
+        this.city["temperature"] = "test";
+        return this.city["temperature"];
+    }
+}

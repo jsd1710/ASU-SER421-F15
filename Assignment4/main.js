ffb10a63936ed957985ed712daa609cd008cd2af
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-10-17 13:15:08 -0700

    Added cookie for APPID

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,10 +1,16 @@
 ﻿var cities = {};
 
+if ( getCookie( "APPID" ) )
+{
+    document.getElementById( "APPID_Form" ).innerHTML = "APPID = " + getCookie( "APPID" );
+}
+
 init();
+
 
 function init()
 {
-    var DOMcities = document.getElementsByClassName("City");
+    DOMcities = document.getElementsByClassName("City");
     for (i = 0; i < DOMcities.length; i++)
     {
         
@@ -29,4 +35,40 @@ function City(name)
         this.city["temperature"] = "test";
         return this.city["temperature"];
     }
+
+    this.updateTimestamp()
+    {
+
+    }
+}
+
+function setAPPID()
+{
+    APPID = document.getElementById( "APPID" ).value;
+    setCookie( "APPID", APPID, 4200 );
+    console.log( "Cookie: APPID set to '" + APPID + "'" );
+    document.getElementById( "APPID_Form" ).innerHTML = "APPID = " + APPID;
+}
+
+function setCookie( cname, cvalue, exdays )
+{
+    var d = new Date();
+    d.setTime( d.getTime() + ( exdays * 24 * 60 * 60 * 1000 ) );
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie( cname )
+{
+    var name = cname + "=";
+    var ca = document.cookie.split( ';' );
+    for ( var i = 0; i < ca.length; i++ )
+    {
+        var c = ca[i];
+        while ( c.charAt( 0 ) == ' ' )
+            c = c.substring( 1 );
+        if ( c.indexOf( name ) == 0 )
+            return c.substring( name.length, c.length );
+    }
+    return null;
 }

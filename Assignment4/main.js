c5f63f0e3f3a285f0ef49d7e46835715dd4703c2
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-10-17 19:26:29 -0700

    Added refresh functionality and started task #2.4

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,44 +1,93 @@
-﻿var cities = {};
+﻿var request = getRequestObject();
+var cities = {};
+var APPID;
+var baseAddress = "http://api.openweathermap.org/data/2.5/weather?q=";
 
 if ( getCookie( "APPID" ) )
 {
-    document.getElementById( "APPID_Form" ).innerHTML = "APPID = " + getCookie( "APPID" );
+    APPID = getCookie( "APPID" );
+    document.getElementById( "APPID_Form" ).innerHTML = "APPID = " + APPID;
 }
 
 init();
 
 
+/* --------------------------------------- */
 function init()
 {
-    DOMcities = document.getElementsByClassName("City");
-    for (i = 0; i < DOMcities.length; i++)
+    var DOMcities = document.getElementsByClassName( "City" );
+    for ( i = 0; i < DOMcities.length-1; i++ )
     {
-        
-        cities[i] = new City(DOMcities[i].children.name.innerHTML);
-        console.log(cities[i].getName());
+        cities[i] = new City( i );
+
+        updateCity( i );
+
+        request.open(
+            "POST",
+            baseAddress + cities[i].getName() + "&APPID=" + APPID,
+            false );
+
+        request.send( null );
     }
 }
 
-function City(name)
+function City( cityIndex )
 {
+    this.DOMCity = document.getElementsByClassName( "City" )[cityIndex];
     this.city = {};
 
-    this.city["name"] = name;
+    this.city["name"] = this.DOMCity.children.name.innerHTML;
 
-    this.getName = function()
+    this.getName = function ()
     {
         return this.city["name"];
     }
 
-    this.getTemp = function()
+    this.setTemp = function (temp)
     {
-        this.city["temperature"] = "test";
+        this.city["temperature"] = temp;
+    }
+    this.getTemp = function ()
+    {
         return this.city["temperature"];
     }
 
-    this.updateTimestamp()
+    this.setHumidity = function(humidity)
     {
+        this.city["humidity"] = humidity;
+    }
+    this.getHumidity = function()
+    {
+        return this.city["humidity"];
+    }
 
+    this.setWindSpeed = function ( temp )
+    {
+        this.city["windspeed"] = temp;
+    }
+    this.getWindSpeed = function ()
+    {
+        return this.city["windspeed"];
+    }
+
+    this.setCloudiness = function ( temp )
+    {
+        this.city["cloudiness"] = temp;
+    }
+    this.getCloudiness = function ()
+    {
+        return this.city["cloudiness"];
+    }
+
+    this.updateTimeStamp = function ()
+    {
+        this.DOMCity.children.time.innerHTML = new Date().toUTCString();
+    }
+    
+    this.setTDElement = function ( attribute, value )
+    {
+        this.DOMCity.children[attribute].innerHTML = value;
+        this.updateTimeStamp()
     }
 }
 
@@ -71,4 +120,59 @@ function getCookie( cname )
             return c.substring( name.length, c.length );
     }
     return null;
+}
+
+function getRequestObject()
+{
+    if ( window.ActiveXObject )
+    {
+        return ( new ActiveXObject( "Microsoft.XMLHTTP" ) );
+    } else if ( window.XMLHttpRequest )
+    {
+        return ( new XMLHttpRequest() );
+    } else
+    {
+        return ( null );
+    }
+}
+
+function updateCity(cityIndex)
+{
+    var weatherJSON;
+    request.onreadystatechange = function ()
+    {
+        if ( ( request.readyState == 4 ) &&
+        ( request.status == 200 ) )
+        {
+             weatherJSON = JSON.parse( request.responseText );
+        }
+    };
+
+    request.open(
+        "POST",
+        baseAddress + cities[cityIndex].getName() + "&APPID=" + APPID,
+        false );
+
+    request.send( null );
+    
+    cities[cityIndex].setTemp( parseFloat( weatherJSON["main"]["temp"] - 273.15 ).toFixed( 2 ) );
+    cities[cityIndex].setTDElement( "temp", cities[cityIndex].getTemp() + "&deg;C" );
+
+    cities[cityIndex].setHumidity( weatherJSON["main"]["humidity"] );
+    cities[cityIndex].setTDElement( "humidity", cities[cityIndex].getHumidity() + "%" );
+
+    cities[cityIndex].setWindSpeed( (parseFloat(weatherJSON["wind"]["speed"]) * (3600) * (1/1609.344)).toFixed(2) );
+    cities[cityIndex].setTDElement( "windspeed", cities[cityIndex].getWindSpeed() + " miles per hour" );
+
+    cities[cityIndex].setCloudiness( weatherJSON["clouds"]["all"] );
+    cities[cityIndex].setTDElement( "cloudiness", cities[cityIndex].getCloudiness() + "%" );
+}
+
+function updateAllCities()
+{
+    var DOMcities = document.getElementsByClassName( "City" );
+    for ( i = 0; i < DOMcities.length-1; i++ )
+    {
+        updateCity( i );
+    }
 }

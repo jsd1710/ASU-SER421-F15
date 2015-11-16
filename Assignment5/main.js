a00fd0f610975f0f34b2bad75152ea198f6ebc16
Jacob Dobkins <jacob.dobkins@outlook.com>
2015-11-15 17:11:58 -0700

    Properly converted app to Angular

--- Assignment5/main.js
+++ Assignment5/main.js
@@ -1,49 +1,40 @@
 ﻿var Assignment = angular.module( 'Assignment5', [] );
 
-Assignment.controller( 'CityController', function ( $scope )
+Assignment.controller( 'CityController', function ( $scope, $http )
 {
-    $scope.cities = [
-        {
-            "name": "Phoenix, US"
-        },
-        {
-            "name": "London, UK"
-        },
-        {
-            "name": "Fulda, DE"
-        }
-    ];
-
-} );
-
-//var request = getRequestObject();
-var hardCodedCities = [
+    $scope.init = function()
     {
-        main: {
-            name: "Phoenix, US",
-            temp: 320.15,
-            humidity: 1,
-        },
-        wind: {
-            speed: 0
-        },
-        clouds: {
-            all: 20
-        }
-    },
-    {
-        main: {
-            name: "London, UK",
-            temp: 300.0,
-            humidity: 90,
-        },
-        wind: {
-            speed: 3
-        },
-        clouds: {
-            all: 0
-        }
-    },
+        console.log( "init()" );
+        $scope.updateAllCities();
+    }
+
+    $scope.hardCodedCities = [
+       {
+           main: {
+               name: "Phoenix, US",
+               temp: 320.15,
+               humidity: 1,
+           },
+           wind: {
+               speed: 0
+           },
+           clouds: {
+               all: 20
+           }
+       },
+       {
+           main: {
+               name: "London, UK",
+               temp: 300.0,
+               humidity: 90,
+           },
+           wind: {
+               speed: 3
+           },
+           clouds: {
+               all: 0
+           }
+       },
     {
         main: {
             name: "Fulda, DE",
@@ -57,10 +48,244 @@ var hardCodedCities = [
             all: 80
         }
     }
-];
-var cities = [];
+    ];
+
+    $scope.cities = [{}, {}, {}];
+
+    $scope.oldCityCall = [{}, {}, {}];
+
+    $scope.City = function ( cityIndex )
+    {
+        this.city = {};
+
+        this.setName = function(name)
+        {
+            this.city["name"] = name;
+        }
+
+        this.getName = function ()
+        {
+            return this.city["name"];
+        }
+
+        this.setTemp = function ( temp )
+        {
+            this.city["temperature"] = temp;
+        }
+        this.getTemp = function ()
+        {
+            return this.city["temperature"];
+        }
+
+        this.setHumidity = function ( humidity )
+        {
+            this.city["humidity"] = humidity;
+        }
+        this.getHumidity = function ()
+        {
+            return this.city["humidity"];
+        }
+
+        this.setWindSpeed = function ( temp )
+        {
+            this.city["windspeed"] = temp;
+        }
+        this.getWindSpeed = function ()
+        {
+            return this.city["windspeed"];
+        }
+
+        this.setCloudiness = function ( temp )
+        {
+            this.city["cloudiness"] = temp;
+        }
+        this.getCloudiness = function ()
+        {
+            return this.city["cloudiness"];
+        }
+
+        this.updateTimeStamp = function ()
+        {
+            this.time = new Date().toLocaleString();
+        }
+    }
+
+    $scope.updateAllCities = function()
+    {
+        console.log( "updateAllCities()" );
+
+        for ( var i = 0; i < $scope.cities.length; i++ )
+        {
+            $scope.cities[i] = new $scope.City( i );
+            $scope.updateCity( i );
+        }
+        
+        $scope.findAvgTemp();
+        $scope.findMaxTemp();
+        $scope.findAvgHumidity();
+        $scope.findMaxHumidity();
+        var qualities = $scope.weatherQuality();
+        $scope.rankWeather( qualities );
+        
+    }
+
+    $scope.updateCity = function ( cityIndex )
+    {
+        var weatherJSON = $scope.hardCodedCities[cityIndex];
+        weatherJSON = $scope.randomize( weatherJSON );
+        /* Previous assignment AJAX call
+        request.onreadystatechange = function ()
+        {
+            if ( ( request.readyState == 4 ) &&
+            ( request.status == 200 ) )
+            {
+                weatherJSON = JSON.parse( request.responseText );
+            }
+        };
+    
+        request.open(
+            "GET",
+            baseAddress + cities[cityIndex].getName() + "&APPID=" + APPID,
+            false );
+    
+        request.send( null );
+        */
+
+        $scope.cities[cityIndex].setName( weatherJSON["main"]["name"] );
+        $scope.cities[cityIndex].setTemp(parseFloat( weatherJSON["main"]["temp"] - 273.15 ).toFixed( 2 ));
+        $scope.cities[cityIndex].setHumidity( weatherJSON["main"]["humidity"] );
+        $scope.cities[cityIndex].setWindSpeed(( parseFloat( weatherJSON["wind"]["speed"] ) * ( 3600 ) * ( 1 / 1609.344 ) ).toFixed( 2 ) );
+        $scope.cities[cityIndex].setCloudiness( weatherJSON["clouds"]["all"] );
+        $scope.cities[cityIndex].updateTimeStamp();
+
+    }
+
+    $scope.randomize = function ( city )
+    {
+        var newCity = { main: {}, clouds: {}, wind: {} };
+        newCity.main.name = city.main.name;
+        newCity.main.temp = city.main.temp + ( city.main.temp - 273.15 ) * Math.random() / 11;
+        newCity.main.humidity = Math.floor( city.main.humidity + Math.random() * 11 );
+        newCity.wind.speed = city.wind.speed + Math.random() * 11 / 3600 * 1609.344;
+        newCity.clouds.all = Math.floor( city.clouds.all + Math.random() * 11 );
+
+        return newCity;
+    }
+
+    $scope.updateAllLastCall = function()
+    {
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            $scope.lastCall( i );
+        }
+    }
+    
+    $scope.clearLastCall = function()
+    {
+        $scope.oldCityCall = [{}, {}, {}];
+        localStorage.removeItem( 'oldCityCall' );
+    }
+    
+    $scope.lastCall = function ( cityIndex )
+    {
+        var old = document.getElementsByClassName( "City" )[cityIndex].children;
+        var oldCity = document.getElementsByClassName( "CityOld" )[cityIndex].children;
+
+        $scope.oldCityCall[cityIndex] = $scope.cities[cityIndex];
+
+        localStorage.setItem( 'oldCityCall', JSON.stringify( $scope.oldCityCall ) );
+
+    }
+
+    
+    if ( JSON.parse( localStorage.getItem( 'oldCityCall' ) ) )
+    {
+        $scope.oldCityCall = JSON.parse( localStorage.getItem( 'oldCityCall' ) );
+    }
+    
+
+    $scope.findAvgTemp = function()
+    {
+        var sum = 0.0;
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            sum += parseFloat( $scope.cities[i].city.temperature );
+        }
+        sum = sum / $scope.cities.length;
+        document.getElementById( "avgTemp" ).innerHTML = sum.toFixed( 2 ) + "&deg;C";
+    }
+    $scope.findMaxTemp = function()
+    {
+        var maxIndex = 0;
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            if ( parseFloat( $scope.cities[i].city.temperature ) > parseFloat( $scope.cities[maxIndex].city.temperature ) )
+            {
+                maxIndex = i;
+            }
+        }
+        document.getElementById( "maxTemp" ).innerHTML = $scope.cities[maxIndex].city.name;
+    }
+
+    $scope.findAvgHumidity = function()
+    {
+        var sum = 0.0;
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            sum += parseFloat( $scope.cities[i].city.humidity );
+        }
+        sum = sum / $scope.cities.length;
+        document.getElementById( "avgHumidity" ).innerHTML = sum.toFixed( 2 ) + "%";
+    }
+    $scope.findMaxHumidity = function()
+    {
+        var maxIndex = 0;
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            if ( parseFloat( $scope.cities[i].city.humidity ) > parseFloat( $scope.cities[maxIndex].city.humidity ) )
+            {
+                maxIndex = i;
+            }
+        }
+        document.getElementById( "maxHumidity" ).innerHTML = $scope.cities[maxIndex].city.name;
+    }
+
+    $scope.weatherQuality = function()
+    {
+        var qualities = [];
+        for ( i = 0; i < $scope.cities.length; i++ )
+        {
+            qualities[i] = parseFloat(
+                $scope.cities[i].city.temperature *
+                ( $scope.cities[i].city.humidity / 100 ) +
+                $scope.cities[i].city.windspeed +
+                $scope.cities[i].city.cloudiness );
+        }
+        return qualities;
+    }
+
+    $scope.rankWeather = function( qualities )
+    {
+        var best = 0;
+        var worst = 0;
+        for ( i = 0; i < qualities.length; i++ )
+        {
+            if ( qualities[i] < qualities[best] )
+            {
+                best = i;
+            }
+            if ( qualities[i] > qualities[worst] )
+            {
+                worst = i;
+            }
+        }
+
+        document.getElementById( "nicest" ).innerHTML = $scope.cities[best].city.name;
+        document.getElementById( "worst" ).innerHTML = $scope.cities[worst].city.name;
+    }
+} );
+
 var APPID;
-var oldCityCall = [{}, {}, {}];
 var baseAddress = "http://api.openweathermap.org/data/2.5/weather?q=";
 
 if ( getCookie( "APPID" ) )
@@ -69,93 +294,15 @@ if ( getCookie( "APPID" ) )
     document.getElementById( "APPID_Form" ).innerHTML = "APPID = " + APPID;
 }
 
-if ( JSON.parse( localStorage.getItem( 'oldCityCall' ) ) )
-{
-    oldCityCall = JSON.parse( localStorage.getItem( 'oldCityCall' ));
-    var DOMcities = document.getElementsByClassName( "City" );
-    for ( i = 0; i < DOMcities.length; i++ )
-    {
-        var oldCity = document.getElementsByClassName( "CityOld" )[i].children;
-
-        oldCity.name.innerHTML = oldCityCall[i]['oldName'] + " (Last Call)";
-        oldCity.temp.innerHTML = oldCityCall[i]['oldTemp'];
-        oldCity.humidity.innerHTML = oldCityCall[i]['oldHumidity'];
-        oldCity.windspeed.innerHTML = oldCityCall[i]['oldWindSpeed'];
-        oldCity.cloudiness.innerHTML = oldCityCall[i]['oldCloudiness'];
-        oldCity.time.innerHTML = oldCityCall[i]['oldTime'];
-    }
-}
 
 angular.element( document ).ready( function ()
 {
-    init();
+
 } );
 
 /* --------------------------------------- */
-function init()
-{
-    updateAllCities();
-}
 
-function City( cityIndex )
-{
-    this.DOMCity = document.getElementsByClassName( "City" )[cityIndex];
-    this.city = {};
 
-    this.city["name"] = this.DOMCity.children.name.innerHTML;
-
-    this.getName = function ()
-    {
-        return this.city["name"];
-    }
-
-    this.setTemp = function (temp)
-    {
-        this.city["temperature"] = temp;
-    }
-    this.getTemp = function ()
-    {
-        return this.city["temperature"];
-    }
-
-    this.setHumidity = function(humidity)
-    {
-        this.city["humidity"] = humidity;
-    }
-    this.getHumidity = function()
-    {
-        return this.city["humidity"];
-    }
-
-    this.setWindSpeed = function ( temp )
-    {
-        this.city["windspeed"] = temp;
-    }
-    this.getWindSpeed = function ()
-    {
-        return this.city["windspeed"];
-    }
-
-    this.setCloudiness = function ( temp )
-    {
-        this.city["cloudiness"] = temp;
-    }
-    this.getCloudiness = function ()
-    {
-        return this.city["cloudiness"];
-    }
-
-    this.updateTimeStamp = function ()
-    {
-        this.DOMCity.children.time.innerHTML = new Date().toLocaleString();
-    }
-    
-    this.setTDElement = function ( attribute, value )
-    {
-        this.DOMCity.children[attribute].innerHTML = value;
-        this.updateTimeStamp()
-    }
-}
 
 function setAPPID()
 {
@@ -188,220 +335,3 @@ function getCookie( cname )
     return null;
 }
 
-/* Previous assignment AJAX call
-function getRequestObject()
-{
-    if ( window.ActiveXObject )
-    {
-        return ( new ActiveXObject( "Microsoft.XMLHTTP" ) );
-    } else if ( window.XMLHttpRequest )
-    {
-        return ( new XMLHttpRequest() );
-    } else
-    {
-        return ( null );
-    }
-}
-*/
-
-function randomize(city)
-{
-    var newCity = { main: {}, clouds: {}, wind: {}};
-    newCity.main.temp = city.main.temp + (city.main.temp - 273.15) * Math.random()/11;
-    newCity.main.humidity = Math.floor(city.main.humidity + Math.random()*11);
-    newCity.wind.speed = city.wind.speed + Math.random() * 11 / 3600 * 1609.344;
-    newCity.clouds.all = Math.floor(city.clouds.all + Math.random()*11);
-
-    return newCity;
-}
-
-function updateCity(cityIndex)
-{
-    var weatherJSON = hardCodedCities[cityIndex];
-    weatherJSON = randomize( weatherJSON );
-    /* Previous assignment AJAX call
-    request.onreadystatechange = function ()
-    {
-        if ( ( request.readyState == 4 ) &&
-        ( request.status == 200 ) )
-        {
-            weatherJSON = JSON.parse( request.responseText );
-        }
-    };
-
-    request.open(
-        "GET",
-        baseAddress + cities[cityIndex].getName() + "&APPID=" + APPID,
-        false );
-
-    request.send( null );
-    */
-
-    cities[cityIndex].setTemp( parseFloat( weatherJSON["main"]["temp"] - 273.15 ).toFixed( 2 ) );
-    cities[cityIndex].setTDElement( "temp", cities[cityIndex].getTemp() + "&deg;C" );
-
-    cities[cityIndex].setHumidity( weatherJSON["main"]["humidity"] );
-    cities[cityIndex].setTDElement( "humidity", cities[cityIndex].getHumidity() + "%" );
-
-    cities[cityIndex].setWindSpeed( (parseFloat(weatherJSON["wind"]["speed"]) * (3600) * (1/1609.344)).toFixed(2) );
-    cities[cityIndex].setTDElement( "windspeed", cities[cityIndex].getWindSpeed() + " miles per hour" );
-
-    cities[cityIndex].setCloudiness( weatherJSON["clouds"]["all"] );
-    cities[cityIndex].setTDElement( "cloudiness", cities[cityIndex].getCloudiness() + "%" );
-}
-
-function updateAllCities()
-{
-    //updateThirdCityRow();
-    var DOMcities = document.getElementsByClassName( "City" );
-    for ( i = 0; i < DOMcities.length; i++ )
-    {
-        cities[i] = new City( i );
-        updateCity( i );
-    }
-    findAvgTemp();
-    findMaxTemp();
-    findAvgHumidity();
-    findMaxHumidity();
-    var qualities = weatherQuality();
-    rankWeather( qualities );
-}
-
-function updateAllLastCall()
-{
-    var DOMcities = document.getElementsByClassName( "City" );
-    for ( i = 0; i < DOMcities.length; i++ )
-    {
-        lastCall( i );
-    }
-}
-
-function clearLastCall()
-{
-    var DOMcities = document.getElementsByClassName( "CityOld" );
-    for ( i = 0; i < DOMcities.length; i++ )
-    {
-        var cityAttributes = DOMcities[i].children;
-        cityAttributes.name.innerHTML = "(Last Call)";
-        cityAttributes.temp.innerHTML = "";
-        cityAttributes.humidity.innerHTML = "";
-        cityAttributes.windspeed.innerHTML = "";
-        cityAttributes.cloudiness.innerHTML = "";
-        cityAttributes.time.innerHTML = "";
-        localStorage.removeItem( 'oldCityCall' );
-    }
-}
-
-function lastCall(cityIndex)
-{
-    var old = document.getElementsByClassName( "City" )[cityIndex].children;
-    var oldCity = document.getElementsByClassName( "CityOld" )[cityIndex].children;
-
-    oldCityCall[cityIndex] = {};
-    oldCityCall[cityIndex]['oldName'] = old.name.innerHTML;
-    oldCityCall[cityIndex]['oldTemp'] = old.temp.innerHTML;
-    oldCityCall[cityIndex]['oldHumidity'] = old.humidity.innerHTML;
-    oldCityCall[cityIndex]['oldWindSpeed'] = old.windspeed.innerHTML;
-    oldCityCall[cityIndex]['oldCloudiness'] = old.cloudiness.innerHTML;
-    oldCityCall[cityIndex]['oldTime'] = old.time.innerHTML;
-
-    localStorage.setItem( 'oldCityCall', JSON.stringify( oldCityCall ) );
-    oldCity.name.innerHTML = oldCityCall[cityIndex]['oldName'] + " (Last Call)";
-    oldCity.temp.innerHTML = oldCityCall[cityIndex]['oldTemp'];
-    oldCity.humidity.innerHTML = oldCityCall[cityIndex]['oldHumidity'];
-    oldCity.windspeed.innerHTML = oldCityCall[cityIndex]['oldWindSpeed'];
-    oldCity.cloudiness.innerHTML = oldCityCall[cityIndex]['oldCloudiness'];
-    oldCity.time.innerHTML = oldCityCall[cityIndex]['oldTime'];
-}
-
-/* 
-function updateThirdCityRow()
-{
-    var citySelect = document.getElementById( "citySelect" );
-    var city = citySelect.options[citySelect.selectedIndex].value;
-
-    var thirdCityRow = document.getElementsByClassName( "City" )[2];
-    thirdCityRow.children.name.innerHTML = city;
-
-}
-*/
-
-function findAvgTemp()
-{
-    var sum = 0.0;
-    for ( i = 0; i < cities.length; i++ )
-    {
-        sum += parseFloat( cities[i].city.temperature );
-    }
-    sum = sum / cities.length;
-    document.getElementById( "avgTemp" ).innerHTML = sum.toFixed(2) + "&deg;C";
-}
-function findMaxTemp()
-{
-    var maxIndex = 0;
-    for ( i = 0; i < cities.length; i++ )
-    {
-        if (parseFloat(cities[i].city.temperature) > parseFloat(cities[maxIndex].city.temperature))
-        {
-            maxIndex = i;
-        }
-    }
-    document.getElementById( "maxTemp" ).innerHTML = cities[maxIndex].city.name;
-}
-
-function findAvgHumidity()
-{
-    var sum = 0.0;
-    for ( i = 0; i < cities.length; i++ )
-    {
-        sum += parseFloat( cities[i].city.humidity );
-    }
-    sum = sum / cities.length;
-    document.getElementById( "avgHumidity" ).innerHTML = sum.toFixed( 2 ) + "%";
-}
-function findMaxHumidity()
-{
-    var maxIndex = 0;
-    for ( i = 0; i < cities.length; i++ )
-    {
-        if ( parseFloat( cities[i].city.humidity ) > parseFloat( cities[maxIndex].city.humidity ) )
-        {
-            maxIndex = i;
-        }
-    }
-    document.getElementById( "maxHumidity" ).innerHTML = cities[maxIndex].city.name;
-}
-
-function weatherQuality()
-{
-    var qualities = [];
-    for (i = 0; i < cities.length; i++)
-    {
-        qualities[i] = parseFloat(
-            cities[i].city.temperature *
-            ( cities[i].city.humidity / 100 ) +
-            cities[i].city.windspeed +
-            cities[i].city.cloudiness);
-    }
-    return qualities;
-}
-
-function rankWeather(qualities)
-{
-    var best = 0;
-    var worst = 0;
-    for (i = 0; i < qualities.length; i++)
-    {
-        if (qualities[i] < qualities[best])
-        {
-            best = i;
-        }
-        if (qualities[i] > qualities[worst])
-        {
-            worst = i;
-        }
-    }
-
-    document.getElementById( "nicest" ).innerHTML = cities[best].city.name;
-    document.getElementById( "worst" ).innerHTML = cities[worst].city.name;
-}
